5085143ff95ecdb015d312802c2ebaba95c59292
DavidGoVa <davidgomez44@aragon.unam.mx>
2022-02-28 13:15:49 -0600

    modificacion a tarea

--- tareas/TareaLibro/src/tarealibro/TareaLibro.java
+++ tareas/TareaLibro/src/tarealibro/TareaLibro.java
@@ -4,6 +4,7 @@
  */
 package tarealibro;
 
+
 /**
  *
  * @author david
@@ -14,8 +15,27 @@ public class TareaLibro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Libro lib1 = new Libro();
+        lib1.setNumPags(228);
+        System.out.println("numero de paginas: "+lib1.getNumPags());
+        lib1.setTitulo("Jueguemos a contar y medir");
+        System.out.println("titulo: "+lib1.getTitulo());
+        lib1.setAutor("Francisco J");
+        System.out.println("Autor: "+lib1.getAutor());
+        lib1.setEditorial("CIME");
+        System.out.println("editorial: "+lib1.getEditorial());
+        lib1.setReleaseDate(2014);
+        System.out.println("año de lanzamiento: "+lib1.getReleaseDate());
+        
+        
+    
+        
+        
+        
+        
         
     }
+
+    
     
 }
