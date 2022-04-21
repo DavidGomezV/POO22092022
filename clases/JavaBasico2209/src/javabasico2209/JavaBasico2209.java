3cd8907a243f28abffc0a38f5c775249c764ea9d
DavidGoVa <davidgomez44@aragon.unam.mx>
2022-04-21 12:48:12 -0500

    clase 21/04/22

--- clases/JavaBasico2209/src/javabasico2209/JavaBasico2209.java
+++ clases/JavaBasico2209/src/javabasico2209/JavaBasico2209.java
@@ -45,6 +45,86 @@ public class JavaBasico2209 {
         JOptionPane.showMessageDialog(null, "Hola mundo", "aqui va el titulo", JOptionPane.ERROR_MESSAGE);
         String dato = JOptionPane.showInputDialog(null, "Dame tu edad", "Introducir datos", JOptionPane.QUESTION_MESSAGE);
         System.out.println(dato);
+        
+        int edad3=15;
+        String resultado = "";
+        if (edad3 <18 ){
+            resultado= "menor de edad";
+        }else{
+            resultado= "mayor de edad";
+        }
+        System.out.println(resultado);
+        
+        //con ternario
+        
+        //<cond>?<true>:<false>;
+        int edad4=25;
+        String res = "";
+        res = edad4 < 18? "Menor de edad" : "Mayor de edad";
+        System.out.println(res);
+        //version minima
+        int edad5= 15;
+        System.out.println(edad5<18?"Menor de edad":"Mayor de edad");
+        
+        int val1=1;
+        int val2=2;
+        
+        //val 1 = 000000...000001;
+        //val 2 = 000000...000010;
+        System.out.println(val1 | val2);
+        
+        int val3=1;
+        int val4=0;
+        
+        val4=val3<<1;
+        
+        System.out.println("------arreglos---------");
+        int[] edades; //[]para ddecir que sera un arreglo aun no reserva memoria
+        edades= new int[5];
+        System.out.println(edades);
+        edades[0]=10;
+        System.out.println("la primer edad es"+edades[0]);
+        
+        //declarar e inicializar en un adola lnea
+        int[]estaturas = new int[5];
+        //se puede asignar directamnete
+        int[]pesos = {89,88,56,76,77};
+         System.out.println(pesos[0]);
+         System.out.println(pesos[1]);
+         System.out.println(pesos[2]);
+         System.out.println(pesos[3]);
+         System.out.println(pesos[4]);
+         
+         //imprimir lo mismo pero con for
+         
+         System.out.println("----------con for---------");
+         for (int i=0; i<pesos.length;i++){
+             System.out.println(pesos[i]);
+         }
+         System.out.println("orden inverso");
+         for (int k=pesos.length-1;k>=0;k--){
+             System.out.println(pesos[k]);
+         }
+         
+        System.out.println("Arreglo de alumnos");
+        Alumno[]lista = new Alumno[5];
+        lista[0]= new Alumno("99999", 2, 9.0f);
+        lista[1]= new Alumno("99998", 2, 7.0f);
+        lista[2]= new Alumno("99997", 3, 10.0f);
+        lista[3]= new Alumno("99996", 2, 5.0f);
+        lista[4]= new Alumno("99995", 2, 9.0f);
+        
+        for (int i=0; i< lista.length;i++){
+            Alumno alumno = lista[i];
+            System.out.println(alumno.evaluarDesempenio());
+        }
+        
+        System.out.println("----for each-------");
+        for (Alumno alumno : lista) {
+            System.out.println(alumno.evaluarDesempenio());
+        }
+        
+            
     }
     
 }
